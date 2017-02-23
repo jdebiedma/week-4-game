19e109dccb86a790870b710bc465517022f16a69
Juan DeBiedma <juanthefatty@gmail.com>
2017-02-23 10:32:58 -0500

    first working attack functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,6 +7,11 @@ var attackPhase = false;
 
 var counter = 0;
 
+var myHealth = 0;
+var myAttack = 0;
+var myCounterAttack = 0;
+var myImageUrl = "";
+var myName = "";
 
 
 var characters = [
@@ -82,7 +87,7 @@ for (var i = 0; i < characters.length; i++)
 }	
 
 
-
+	//CHARACTER SELECT
 
 	$('.char-example').on("click", function(){
 				if (gameStart) {
@@ -112,23 +117,57 @@ for (var i = 0; i < characters.length; i++)
       								.append("<h5>HP = " + characters[i].health + "</h5>")
       								.appendTo("#redbox");
 
+      								
+
 							
 												
 						}
-					}
+							if (characters[i].name === chosenChar) {
 
+								myName = characters[i].name;
+								myImageUrl = characters[i].imageUrl;
+								myHealth = characters[i].health;
+      							myAttack = characters[i].attack;
+      							myCounterAttack = characters[i].counterAttack;
+
+
+							}
+
+
+
+
+					}
+						//ENEMY SELECT
 
 						$(".enemy1").on("click", function() {
 							if (enemySelect) {
 									chosenEnemy = $(this).data('name');
 									
-											//Here's where the attack phase goes
+											
 
 									for (var i = 0; i < characters.length; i++) {
 													
 
 									if (characters[i].name === chosenEnemy) {
-												
+									
+
+									var yourCharacter = $("<div>")
+									.html("<h4>" + myName + "</h4>")
+      								.prepend('<img src=' + myImageUrl +' width="auto" height="125">')
+      								.data("chosen",  true)
+      								.removeClass("space")
+      								.addClass("selection")
+      								.data("name", myName)
+      								.append("<h5 id ='myHealthHolder'>HP = " + myHealth + "</h5>")
+      								.append("<h3>Player</h3>")
+      								.appendTo("#blackbox");
+
+      								var vsHolder = $("<div>")
+      								.html("<h2>" + "VERSUS" + "</h2>")
+      								.addClass("vsIcon")
+      								.appendTo("#blackbox");
+
+
 									var selectedEnemy = $("<div>")
 									.html("<h4>" + characters[i].name + "</h4>")
       								.prepend('<img src=' + characters[i].imageUrl +' width="auto" height="125">')
@@ -136,7 +175,7 @@ for (var i = 0; i < characters.length; i++)
       								.removeClass("space")
       								.addClass("yourOpponent")
       								.data("name", characters[i].name)
-      								.append("<h5>HP = " + characters[i].health + "</h5>")
+      								.append("<h5 id ='enemyHealthHolder'>HP = " + characters[i].health + "</h5>")
       								.append("<button type='button' class='btn btn-success' id='attackButton'>Attack</button>")
       								.appendTo("#blackbox");
 
@@ -150,17 +189,26 @@ for (var i = 0; i < characters.length; i++)
 												attackPhase = true;
 
 												
-
-
+												// ATTACK PHASE 1
+												// you might be able to reference things outside of functions with their 
+												// global variable tags...
 
 												$('#attackButton').on("click", function(){
-
+												if (attackPhase)	{
 												for (var i = 0; i < characters.length; i++) {
 												if (characters[i].name === chosenEnemy){
 
-														alert("you attacked " + characters[i].name);
-														characters[i].health 
+														characters[i].health = characters[i].health - myAttack;
+														myHealth = myHealth - characters[i].counterAttack;
+														
+														$("#enemyHealthHolder").html("<h5 id ='enemyHealthHolder'>HP = " 
+															+ characters[i].health + "</h5>");
 
+														$("#myHealthHolder").html("<h5 id ='myHealthHolder'>HP = " 
+															+ myHealth + "</h5>");
+														
+
+												}
 												}
 												}
 												});
